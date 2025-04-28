5dcd109456814115aa9647df8e545ee0252c165a
Laurits7 <laurits.tani@gmail.com>
2025-04-28 12:37:24 +0200

    checkpoint - setting initial logic

---
+++ ml4cc/scripts/train_all_scenarios.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# This script trains all scenarios for the ML4CC project.
+
+# Cases: CEPC, FCC
+# FCC_energies = 2, 4, 6 etc etc
+# CEPC_energies = ...
+# CEPC_particles
+
+# Training_types: one-step, two-step, two_step_minimal
+
+# Two_step CL models: CNN, DNN, RNN, (DGCNN)
